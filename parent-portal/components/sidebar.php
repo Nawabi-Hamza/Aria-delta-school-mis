64381c8a3a3be86a28c6e26c333f607ff7ad9b5a
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2025-02-18 14:12:09 +0430

    update finance icon

--- parent-portal/components/sidebar.php
+++ parent-portal/components/sidebar.php
@@ -12,7 +12,7 @@
     <li class="side-item"><a href="#" id="sidebarButton" data-target="pages/index.php"><span><i class="bi bi-house h4"></i> </span><span>Home</span></a></li>
     <li class="side-item"><a href="#" id="sidebarButton" data-target="pages/assignments.php"><span><i class="bi bi-people-fill h4"></i> </span><span>Assignments</span></a></li>
     <li class="side-item"><a href="#" id="sidebarButton" data-target="pages/attendance.php"><span><i class="bi bi-person-workspace h4"></i> </span><span>Attendance</span></a></li>
-    <li class="side-item"><a href="#" id="sidebarButton" data-target="pages/finance.php"><span><i class="bi bi-person-video h4"></i> </span><span>Finance</span></a></li>
+    <li class="side-item"><a href="#" id="sidebarButton" data-target="pages/finance.php"><span><i class="bi bi-cash-coin h4"></i> </span><span>Finance</span></a></li>
     <li class="side-item"><a href="#" id="sidebarButton" data-target="pages/signout.php"><span><i class="bi bi-lock h4"></i> </span><span>Logout</span></a></li>
 </ul>
 
